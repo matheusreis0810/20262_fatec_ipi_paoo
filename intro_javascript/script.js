911c2492d01fa111d03e95c6ae1ce467bfe4cf0f
Matheus Reis <souzareis42@gmail.com>
2026-08-05 17:39:23 -0300

    learn: dupla passagem

--- intro_javascript/script.js
+++ intro_javascript/script.js
@@ -1,7 +1,32 @@
-var nome = 'José'
+//PROBLEMA EM UTILIZAR O VAR
+
+//COM O VAR DEIXA EXECUTAR ESSE CODIGO
+//NO PRIMEIRO CONSOLE.LOG APARECE UNDEFINED 
+//E A DECLARACAO DENTRO DO IF E COMO SE FOSSE DECLARADA NO COMECO DO CODIGO, 
+// FAZENDO COM Q O ULTIMO LOG APAREÇA O NOME
+//COM O LET NAO DEIXA EXECUTAR, POIS O QUE É DECLARADO DENTRO DO IF FUNCIONA SO DENTRO DELE
+var idade = 18
+console.log("Oi, " + nome)
+if(idade >=18){
+    var nome = "João"
+    console.log("Parabens, " + nome + ". Voce pode dirigir")
+}
+console.log("Ate mais, " + nome)
+
+/* //ELE DEIXA DECLARAR MAIS DE UMA VARIAVEL COM O MESMO NOME, QUE NO CASO REDEFINIR ELA
+//SE DEIXAR COMO LET NAO DEIXA
+var linguagem = "Javascript"
+console.log("Aprendendo " + linguagem)
+var linguagem = "Java"
+console.log("Apredendo " + linguagem) */
+
+
+//DECLARACAO DE VARIAVEIS
+
+/* var nome = 'José'
 console.log(nome)
 nome = "João"
-console.log(nome)
+console.log(nome) */
 
 /* let nome = "Ana"
 console.log(nome)
