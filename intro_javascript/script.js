5edc526fde1c71d2dd283c3ffb230beed21a7f71
Matheus Reis <souzareis42@gmail.com>
2026-08-05 16:21:57 -0300

    learn: sistema de tipos

--- intro_javascript/script.js
+++ intro_javascript/script.js
@@ -1,1 +1,15 @@
 //comecando...
+//declaracao de variavel -> const, let, var e tambem pode deixar sem nada
+//as variaveis nao tem tipo, elas sao tipo ponteiros q apontam para objs
+//sao variaveis de referencia
+/* a = 2
+console.log(typeof(a))
+a = "abc"
+console.log(typeof(a))
+a.falar() 
+
+//em java seria
+int a = 2;
+a= "abc";
+a.falar;
+*/
